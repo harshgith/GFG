2d6bb28e30c98f56aae2e505fb425ce60d0afac3
harshgith <96532749+harshgith@users.noreply.github.com>
2024-09-25 21:18:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Palindrome Linked List/palindrome-linked-list.py
@@ -0,0 +1,59 @@
+class Solution:
+    def isPalindrome(self, head):
+        lst = []
+        while head.next!=None:
+            lst.append(head.data)
+            head = head.next
+        lst.append(head.data)
+        if lst[::1] == lst[::-1]:
+            return True
+        else:
+            return False
+#{ 
+ # Driver Code Starts
+#main
+
+
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Utility function to prit the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print("")
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        values = input().strip().split()
+        for i in reversed(values):
+            llist.push(i)
+        flag = Solution().isPalindrome(llist.head)
+        if flag:
+            print("true")
+        else:
+            print("false")
+        t -= 1
+# Contributed by: Harshit Sidhwa
+
+# } Driver Code Ends
